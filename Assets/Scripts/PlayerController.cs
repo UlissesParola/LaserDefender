b7ec78bd920babc659a5f7153e7e70354ec36d8c
Ulisses Parola <ulissesmt@gmail.com>
2017-08-30 23:48:46 -0300

    tweak the moviment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine.Assertions.Comparers;
 public class PlayerController : MonoBehaviour
 {
 
-	public float SpaceShipVelocity = 7f;
+	public float SpaceShipVelocity = 15f;
 
 	private Rigidbody2D _playerRigidbody2D;
 	private float _minX = -6.5f;
@@ -23,17 +23,17 @@ public class PlayerController : MonoBehaviour
 		
 		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
 		{
-			_playerRigidbody2D.velocity = new Vector2(-SpaceShipVelocity, 0f);
-			Debug.Log("Left");
+			//transform.position += new Vector3(-SpaceShipVelocity, 0f, 0f);
+			_playerRigidbody2D.velocity =  new Vector3(-SpaceShipVelocity, 0f, 0f);
 		}
 		
 		if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
 		{
-			_playerRigidbody2D.velocity = new Vector2(SpaceShipVelocity, 0f);
-			Debug.Log("Right");
+			//transform.position += new Vector3(SpaceShipVelocity, 0f, 0f);
+			_playerRigidbody2D.velocity =  new Vector3(SpaceShipVelocity, 0f, 0f);
 		}
 		
 		Vector3 positionLimits = new Vector3(Mathf.Clamp(gameObject.transform.position.x, _minX, _maxX), gameObject.transform.position.y, 0);
-		gameObject.transform.position =positionLimits ;
+		gameObject.transform.position = positionLimits ;
 	}
 }
