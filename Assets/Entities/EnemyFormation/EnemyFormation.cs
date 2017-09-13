857fd07eea401c67a0f4cafa0ea2ee65ef088adc
Ulisses Parola <ulissesmt@gmail.com>
2017-09-12 22:23:23 -0300

    Added movement to the enemies formation

---
+++ Assets/Entities/EnemyFormation/EnemyFormation.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFormation : MonoBehaviour
+{
+
+	public GameObject EnemyPrefab;
+	public float GizmoWidth = 6.74f;
+	public float GizmoHeight = 4.42f;
+	public float Speed = 5f;
+
+	private float _xMax;
+	private float _xMin;
+	private bool movingRight = true;
+
+	
+	// Use this for initialization
+	void Start ()
+	{
+		GetScreenLimits();
+		
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			Transform child = this.transform.GetChild(i);
+			GameObject enemy = Instantiate(EnemyPrefab, child.position, Quaternion.identity);
+			enemy.transform.parent = child.transform;
+		}
+		
+		/*foreach (Transform child in this.transform)
+		{
+			GameObject enemy = Instantiate(EnemyPrefab, child.position, Quaternion.identity);
+			enemy.transform.parent = child.transform;
+		}*/
+
+	}
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.DrawWireCube(transform.position, new Vector3(GizmoWidth, GizmoHeight));
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.Log(movingRight);
+		if (movingRight)
+		{
+			transform.position += Vector3.right * Speed * Time.deltaTime;
+		}
+		else
+		{
+			transform.position += Vector3.left * Speed * Time.deltaTime;
+		}
+		
+		float widthPadding = GizmoWidth * 0.5f;
+
+		                     
+		if ((transform.position.x + widthPadding) > _xMax || (transform.position.x - widthPadding) < _xMin )
+		{
+			movingRight = !movingRight;
+		}
+		
+	}
+
+	private void GetScreenLimits()
+	{
+		float distance = Camera.main.transform.position.z - gameObject.transform.position.z;
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, distance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0f, distance));
+
+		_xMin = leftMost.x;
+		_xMax = rightMost.x;
+	}
+}
