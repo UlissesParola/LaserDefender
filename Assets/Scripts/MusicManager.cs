e80155eff171010cbf1c5bf80d3a68689c64f97e
Ulisses Parola <ulissesmt@gmail.com>
2017-09-18 09:41:28 -0300

    tweak MusicManager

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -7,17 +7,12 @@ public class MusicManager : MonoBehaviour
 {
 	public AudioClip Clip;
 	
-	private static Scene _oldScene;
 	private static Scene _activeScene;
 
 	// Use this for initialization
 	private void Awake()
 	{
-		_oldScene = _activeScene;
 		_activeScene = SceneManager.GetActiveScene();
-		
-		Debug.Log(_oldScene.name);
-		Debug.Log(_activeScene.name);
 
 		if (_activeScene.name == "Start Menu" && !GameObject.Find("MusicPlayer").GetComponent<AudioSource>().isPlaying)
 		{
